9701acf7b31e349b036d11f5cf93779fd2f27b03
Sarvesh Shah <42857596+sarveshshah@users.noreply.github.com>
2020-10-10 18:22:09 -0400

    added a bat file for quick execution

---
+++ run.bat
@@ -0,0 +1,2 @@
+@echo off
+cmd /k "cd /d C:\Users\asvh\Documents\GitHub\Rubrics Builder Web app\.venv\Scripts\ & activate & cd /d  C:\Users\asvh\Documents\GitHub\Rubrics Builder Web app\ & streamlit run main.py"
