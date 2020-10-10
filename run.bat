0086f3a459f6c72f2e52e5438b7aa1fc6b9a9905
Sarvesh Shah <42857596+sarveshshah@users.noreply.github.com>
2020-10-10 18:41:41 -0400

    Renaming file for better end user Experience.

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
 @echo off
-cmd /k "cd /d C:\Users\asvh\Documents\GitHub\Rubrics Builder Web app\.venv\Scripts\ & activate & cd /d  C:\Users\asvh\Documents\GitHub\Rubrics Builder Web app\ & streamlit run main.py"
+cmd /k "cd /d C:\Users\asvh\Documents\GitHub\Rubrics Builder Web app\.venv\Scripts\ & activate & cd /d  %~dp0 & streamlit run Rubrics Builder.py"
